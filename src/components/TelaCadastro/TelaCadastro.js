e6be74ba10aa272af4b163d6b95b7622356b688f
mari-trancoso <mnegraotrancoso@gmail.com>
2022-10-25 13:36:10 -0300

    exercicios da tarde

--- src/components/TelaCadastro/TelaCadastro.js
+++ src/components/TelaCadastro/TelaCadastro.js
@@ -21,7 +21,7 @@ function TelaCadastro(props) {
           Confirmação da senha:
           <Input id="descricao" />
         </StyledLabel>
-        <SendButton onClick={() => props.mudarTela(3)}>Cadastrar</SendButton>
+        <SendButton onClick={() => props.mudarTela(3)}>Cadastrar Endereço</SendButton>
         <BackToLoginButton onClick={() => props.mudarTela(1)}>Já possuo um cadastro</BackToLoginButton>
       </Form>
     </FormContainer>
